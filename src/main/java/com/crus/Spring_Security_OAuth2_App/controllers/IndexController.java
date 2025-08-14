f45183d577c998e04d7631df073b95f1eed99262
v605690 <v605690@gmail.com>
2025-08-14 09:53:27 -0500

    Add and update project, IndexController, SecurityConfig, application.properties, index.html

---
+++ src/main/java/com/crus/Spring_Security_OAuth2_App/controllers/IndexController.java
@@ -0,0 +1,21 @@
+package com.crus.Spring_Security_OAuth2_App.controllers;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(Authentication authentication) {
+        if (authentication != null) {
+            System.out.println(authentication);
+        }
+        return "index";
+    }
+
+    @GetMapping("/protected")
+    public String protectedIndex() {
+        return "index";
+    }
+}
