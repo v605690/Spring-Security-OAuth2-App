3823b3f9f029a7c22ae064b10aa0a59dba85344d
v605690 <v605690@gmail.com>
2025-08-14 08:40:14 -0500

    Add new Spring-Security-OAuth2-Project

---
+++ src/main/java/com/crus/Spring_Security_OAuth2_App/SpringSecurityOAuth2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.crus.Spring_Security_OAuth2_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuth2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOAuth2ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crus/Spring_Security_OAuth2_App/SpringSecurityOAuth2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crus.Spring_Security_OAuth2_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOAuth2ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
